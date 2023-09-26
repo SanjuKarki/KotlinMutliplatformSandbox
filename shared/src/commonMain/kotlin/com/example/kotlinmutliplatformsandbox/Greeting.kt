a9789c242aa017f3a9ef66adb6b3d3e8def68961
SanjuKarki. <A00278807@mycambrian.ca.>
2023-09-26 18:40:30 -0400

    First

---
+++ shared/src/commonMain/kotlin/com/example/kotlinmutliplatformsandbox/Greeting.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinmutliplatformsandbox
+import kotlin.random.Random
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): List<String> = buildList {
+        add(if (Random.nextBoolean()) "Hi!" else "Hello!")
+        add("Guess what it is! > ${platform.name.reversed()}!")
+    }
+}
